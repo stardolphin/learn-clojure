a769127bbaf157de22056e49cd968002c242f5d5
stardolphin <dolphinlink@gmail.com>
2020-04-26 17:24:20 -0600

    Checking!

--- src/learn_clojure/core.clj
+++ src/learn_clojure/core.clj
@@ -1,4 +1,6 @@
 
+
+
 (ns learn-clojure.core
   (:gen-class))
 
@@ -7,6 +9,47 @@
   [& args]
   (println "Hello, World!"))
 
-(defn square [x] (* x x))
+;; multi-variate recursive example
+(defn my-sum
+  ([vals] (my-sum 0 vals))
+  ([total vals] 
+   (if (empty? vals)
+     total
+     (recur (+ (first vals) total) (rest vals)))))
 
-(square 3)
+(my-sum [1 2 3 4 5])
+
+(defn thing "Pink moon!" [] (println "PINK!")
+  )
+(thing)
+
+(reduce (fn [total vals] (+ total vals)) 0 [0 1 2 3 4] )
+(reduce (fn [total vals] (* total vals)) 1 [1 2 3 4 5])
+
+(take 15 (cycle [1 2 3 4]))
+
+(defn my-zipmap [keys vals]
+  (loop [my-map {}
+         my-keys (seq keys)
+         my-vals (seq vals)]
+    (if (and my-keys my-vals)
+      (recur (assoc my-map (first my-keys) (first my-vals))
+             (next my-keys)
+             (next my-vals))
+      my-map)))
+
+(println (my-zipmap [:a :b :c] [1 2 3]))
+
+(defun my-command ()
+  (interactive)
+  (save-excursion)
+  (do-some-things)
+  (do-more-things)
+  (conclude-doing-things a-string)
+  )
+
+;; 
+
+;; http://danmidwood.com/content/2014/11/21/animated-paredit.html
+;; https://www.youtube.com/watch?v=D6h5dFyyUX0   - paredit mode
+;; https://youtu.be/AmP6rW30wjE - Some database access
