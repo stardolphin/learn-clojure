7b5325e2a03b2cb24521abd1d0bcb805657bff30
stardolphin <dolphinlink@gmail.com>
2020-05-04 22:03:14 -0600

    Added println of said array

--- src/learn_clojure/interacting_with_java.clj
+++ src/learn_clojure/interacting_with_java.clj
@@ -31,4 +31,37 @@
 ;;
 (def my-array (into-array String ["Doctor" "my" "eyes"]))
 (aget my-array 0)
+(ns learn-clojure.interacting-with-java
+  (:require [clojure.string :as str])
+  (:import [java.util Date Calendar]))
 
+(def date (str (Date. (System/currentTimeMillis))))
+
+(println date)
+
+(def millis (System/currentTimeMillis))
+
+(println long millis)
+
+;;
+(Date. (System/currentTimeMillis))
+
+;;
+(. (Date.) getTime)
+
+;;
+(doto (Calendar/getInstance)
+  (.set Calendar/YEAR 1985)
+  (.set Calendar/MONTH 05)
+  (.set Calendar/DATE 04))
+
+;;
+(int-array 10)
+
+;; try ^<space> to expand commands
+(str/capitalize "FoO fOf")
+
+;;
+(def my-array (into-array String ["Doctor" "my" "eyes" "have" "seen" "the" "years"]))
+(aget my-array 0)
+(map println (seq my-array))
