e7a5dad5f9bece73bdfd0f23a1cfedcb632db014
stardolphin <dolphinlink@gmail.com>
2020-05-11 07:12:03 -0600

    Still working on concurrency

--- src/learn_clojure/basic_concurrency.clj
+++ src/learn_clojure/basic_concurrency.clj
@@ -48,3 +48,12 @@
 (defn my-function-with-delay [] (delay (slow-log "Delayed Message")) :ok)
 (my-function-with-delay)
 
+(defn fib [n] (if (< n 2) 
+                1
+                (+ (fib (- n 1)) (fib (- n 2)))))
+
+(defn fib [n] (if (< n 2)
+                1
+                (+ (fib (- n 1)) (fib (- n 2)))))
+
+(println (fib 40))
