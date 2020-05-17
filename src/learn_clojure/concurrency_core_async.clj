3cb511f22ee688e4211830e6b45584643c0f8e54
stardolphin <dolphinlink@gmail.com>
2020-05-17 17:29:33 -0600

    Trying loops and patterns of core-async

--- src/learn_clojure/concurrency_core_async.clj
+++ src/learn_clojure/concurrency_core_async.clj
@@ -1,5 +1,5 @@
 (ns learn-clojure.concurrency-core-async
-  (:require [clojure.core.async :refer [chan go >! <! go-loop] ]))
+  (:require [clojure.core.async :refer [chan go >! <! go-loop]]))
 
 ;; chan can take a buffer, a transform, a number implying number of messages
 (def my-chan (chan))
@@ -18,3 +18,11 @@
 (go (>! my-chan "Hello"))
 
 
+(defn print-listener [chan]
+  (go-loop []
+    (println (<! chan))
+    (recur))
+  chan)
+
+(def new-chan (chan))
+(print-listener new-chan)
