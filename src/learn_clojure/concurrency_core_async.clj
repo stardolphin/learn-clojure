f7904966626a0cce2710a11f53d54e5f9ef101dd
stardolphin <dolphinlink@gmail.com>
2020-05-19 19:42:15 -0600

    renamed some variables here

--- src/learn_clojure/concurrency_core_async.clj
+++ src/learn_clojure/concurrency_core_async.clj
@@ -38,19 +38,19 @@
 (print-listener rev-chan)
 (go (>! in-chan [1 2 3]))
 
-(defn combine-channels [& chans]
+(defn combine-channels [& channels]
   (let [out-chan (chan)]
     (go-loop []
-      (let [[value c] (alts! chans)] (>! out-chan value))
+      (let [[value _] (alts! channels)] (>! out-chan value))
       (recur))
     out-chan)
   )
 (def chan1 (chan))
 (def chan2 (chan))
 
-(def both-chans (combine-channels chan1 chan2))
+(def both-channels (combine-channels chan1 chan2))
 
-(print-listener both-chans)
+(print-listener both-channels)
 
 (go (>! chan1 "Hi chan1!"))
 (go (>! chan2 "Hi chan2!"))
