c3b09cb3375b322aeac5c5356ef64da0e3343e79
stardolphin <dolphinlink@gmail.com>
2020-05-23 18:49:35 -0600

    Bunch of typedef examples.

--- src/learn_clojure/types_and_records.clj
+++ src/learn_clojure/types_and_records.clj
@@ -7,3 +7,21 @@
 
 (println (.name (->MyPerson "Haystack")))
 
+(defrecord MyPersonRecord [name])
+
+;; #learning_clojure.core.MyPersonRecord{:name "Jordan"}
+(->MyPersonRecord "Jordan")
+
+;; Treat these as a map
+(println (:name (->MyPersonRecord "Jordan")))
+
+;; Possible to create a map directly from one of these
+(println (:name (map->MyPersonRecord {:name "Freddy"})))
+
+(deftype MyRunnable [name]
+  Runnable
+  (run [this] (println (.name this))))
+
+;; Not sure, but defines a runnable in Java
+(.run (MyRunnable. "Francine"))
+
