52045a45d0757cab019188ff25d51dd2351181d6
stardolphin <dolphinlink@gmail.com>
2020-05-30 11:09:47 -0600

    added just one file

--- src/learn_clojure/reading_and_writing_files.clj
+++ src/learn_clojure/reading_and_writing_files.clj
@@ -1,7 +1,20 @@
-(ns learn-clojure.reading-and-writing-files )
-
+(ns learn-clojure.reading-and-writing-files)
 
 (println (+ 1 1))
 
-(def my-file-path "/home/michael/Sites/learning-clojure/file.txt")
+(def file-path-one "/home/michael/Sites/learn-clojure/spat.txt")
 
+(println file-path-one)
+
+(spit file-path-one (repeat 5 "HELLO!!!!!!"))
+
+(println (count (slurp file-path-one)))
+
+(require '[clojure.java.io :as io])
+
+(def file-path-two "/home/michael/Sites/learn-clojure/forward")
+
+(with-open [reader (io/reader file-path-two)
+            writer (io/writer (str file-path-two ".out.txt"))]
+  (doall
+    (map #(.write writer (apply str (reverse %))) (line-seq reader))))
