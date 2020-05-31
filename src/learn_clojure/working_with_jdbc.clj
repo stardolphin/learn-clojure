759b65e31c0463b08f5ecfb31f4802d9a6f61219
stardolphin <dolphinlink@gmail.com>
2020-05-31 15:29:53 -0600

    Now connects to a db

--- src/learn_clojure/working_with_jdbc.clj
+++ src/learn_clojure/working_with_jdbc.clj
@@ -1,14 +1,22 @@
 (ns learn-clojure.working-with-jdbc
   (:require
     [clojure.java.jdbc :as db]
-    [jdbc.pool.c3p0    :as pool]
+    [jdbc.pool.c3p0 :as pool]
     ))
 (def my-db {:subprotocol "postgresql"
-            :subname "//127.0.0.1:5432/clojure_test"
-            :user "michael"
-            :password "michael"
+            :subname     "//127.0.0.1:5432/clojure_test"
+            :user        "michael"
+            :password    "michael"
             })
 (def my-pool (pool/make-datasource-spec my-db))
 
-(db/execute! my-pool ["DROP TABLE IF EXISTS employees"])
+;; (db/execute! my-pool ["DROP TABLE IF EXISTS employees"])
 
+;(db/execute! my-pool ["CREATE TABLE employees (
+;  id serial PRIMARY KEY,
+;  name text,
+;  email text)"])
+
+(db/execute! my-pool
+             ["INSERT INTO employees (name, email) VALUES (?,?)"
+              "Tucker" "tucker@doggo.bone"])
