53a61bc07e94ab3a6ee6c225fc2577018392775e
stardolphin <dolphinlink@gmail.com>
2020-05-31 01:03:37 -0600

    Works enough

--- project.clj
+++ project.clj
@@ -2,12 +2,14 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/data.csv "1.0.0"]
                  [org.clojure/core.async "1.2.603"]
-                 [org.clojure/java.jdbc "0.7.11"]
-                 [org.postgresql/postgresql "9.4-1200-jdbc41"] ]
+                 [org.postgresql/postgresql "9.4-1200-jdbc41"]
+                 [org.clojure/java.jdbc "0.3.6"]
+                 [clojure.jdbc/clojure.jdbc-c3p0 "0.3.1"]
+                 ]
   :main ^:skip-aot learning-clojure.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
--- src/learn_clojure/working_with_jdbc.clj
+++ src/learn_clojure/working_with_jdbc.clj
@@ -1,3 +1,6 @@
-(ns learn-clojure.working-with-jdbc)
-
+(ns learn-clojure.working-with-jdbc
+  (:require
+    [clojure.java.jdbc :as db]
+    [jdbc.pool.c3p0    :as pool]
+    ))
 
