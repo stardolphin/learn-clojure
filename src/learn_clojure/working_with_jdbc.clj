3f69d36988b0fd37ccec9182dea9e3ebfc201030
stardolphin <dolphinlink@gmail.com>
2020-06-01 06:54:50 -0600

    More queries.

--- src/learn_clojure/working_with_jdbc.clj
+++ src/learn_clojure/working_with_jdbc.clj
@@ -3,11 +3,13 @@
     [clojure.java.jdbc :as db]
     [jdbc.pool.c3p0 :as pool]
     ))
+
 (def my-db {:subprotocol "postgresql"
             :subname     "//127.0.0.1:5432/clojure_test"
             :user        "michael"
             :password    "michael"
             })
+
 (def my-pool (pool/make-datasource-spec my-db))
 
 ;; (db/execute! my-pool ["DROP TABLE IF EXISTS employees"])
